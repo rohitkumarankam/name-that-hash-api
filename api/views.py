b4b167eabd6476fd7364d4dbdf72522f19a35d41
rohitkumarankam <root@rka.li>
2022-01-04 13:49:01 +0530

    changed hash processing

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse
 from name_that_hash import runner
+import base64
 
 # Create your views here.
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
         <p>to use that api send http request to http://nth.rka.li/<hash></p>
         <p>example <a href="http://nth.rka.li/2d235ace000a3ad85f590e321c89bb99">http://nth.rka.li/2d235ace000a3ad85f590e321c89bb99</a></p>""")
 def hash(request, hash):
-    hashes = [str(hash)]
-    output = runner.api_return_hashes_as_json(hashes, {"popular_only": True})
+    base64str = base64.b64encode(hash.encode('utf-8'))
+    hasharray = [str(base64str.decode('utf-8'))]
+    output = runner.api_return_hashes_as_json(hasharray, {"base64": True ,"popular_only": True})
     return HttpResponse(output)
