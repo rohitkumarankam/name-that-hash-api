9ecaa63193f9a0a7d3f12fd3805bdfe2df268149
rohitkumarankam <root@rka.li>
2022-01-04 13:32:11 +0530

    created api

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import HttpResponse
+from name_that_hash import runner
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the api index.")
+    return HttpResponse("""
+        <h1>welcome to Name-That-Hash api</h1>
+        <p>to use that api send http request to http://nth.rka.li/<hash></p>
+        <p>example <a href="http://nth.rka.li/2d235ace000a3ad85f590e321c89bb99">http://nth.rka.li/2d235ace000a3ad85f590e321c89bb99</a></p>""")
 def hash(request, hash):
-    return HttpResponse("Hello, world. You're at the api hash."+hash)
+    hashes = [str(hash)]
+    output = runner.api_return_hashes_as_json(hashes, {"popular_only": True})
+    return HttpResponse(output)
